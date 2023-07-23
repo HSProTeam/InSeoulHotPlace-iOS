1f6e8de3d6d3c521583fd813505db810cc36ff6c
Jiseok97 <wltjr1426@naver.com>
2023-07-23 15:47:52 +0900

    [Add] 프로젝트 생성

---
+++ InSeoulHotPlace-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InSeoulHotPlace-iOS
+//
+//  Created by 이지석 on 2023/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
