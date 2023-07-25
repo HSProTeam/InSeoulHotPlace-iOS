60a10cf5484eb1dcc53e375f1f142e0118102d32
Jiseok97 <wltjr1426@naver.com>
2023-07-25 23:31:55 +0900

    [Feat] APIConstant → baseURL, baseGetCategory

--- InSeoulHotPlace-iOS/Sources/Common/APIConstant.swift
+++ InSeoulHotPlace-iOS/Sources/Common/APIConstant.swift
@@ -5,4 +5,9 @@
 //  Created by 이지석 on 2023/07/25.
 //
 
-import Foundation
+struct APIConstant {
+    static let baseURL = "https://data.seoul.go.kr/SeoulRtd/getCategoryList"
+    
+    // Basic path
+    static let getCategory = "?page=1&category=전체보기&count=113&sort=true"
+}
