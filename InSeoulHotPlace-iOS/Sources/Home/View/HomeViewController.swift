388b6539713ba6ba2c98c48a24edc1bb8e91ca81
Jiseok97 <wltjr1426@naver.com>
2023-07-25 23:13:10 +0900

    [Feat] HomeViewController → SearchTextFieldView 추가

--- InSeoulHotPlace-iOS/Sources/Home/View/HomeViewController.swift
+++ InSeoulHotPlace-iOS/Sources/Home/View/HomeViewController.swift
@@ -9,27 +9,44 @@ import UIKit
 
 final class HomeViewController: BaseViewController {
     
+    private let searchTextFieldView = SearchTextFieldView()
     private let locationTableView = UITableView().then {
         $0.backgroundColor = .systemGreen
     }
     
     override func setupViews() {
+        view.backgroundColor = .white
+        view.addSubview(searchTextFieldView)
+        view.addSubview(locationTableView)
+        
         locationTableView.delegate = self
         locationTableView.dataSource = self
         locationTableView.register(LocationTableViewCell.self, forCellReuseIdentifier: "LocationTableViewCell")
         
-        view.addSubview(locationTableView)
+        searchTextFieldView.delegate = self
     }
     
     override func setupLayouts() {
+        searchTextFieldView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(4)
+            $0.leading.trailing.equalToSuperview()
+        }
+        
         locationTableView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(8)
+            $0.top.equalTo(searchTextFieldView.snp.bottom).offset(8)
             $0.leading.trailing.bottom.equalToSuperview()
         }
     }
     
     override func setupBinding() {
         
+    }
+}
+
+// MARK: - SearchTextFieldView Delegate
+extension HomeViewController: SearchTextFieldDelegate {
+    func searchText(title: String) {
+        print("DEBUG: title is \(title)")
     }
 }
 
