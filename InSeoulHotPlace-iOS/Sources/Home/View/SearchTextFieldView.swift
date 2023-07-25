f5b5efd55997d307c025918c77a74ac0d51c8c9a
Jiseok97 <wltjr1426@naver.com>
2023-07-25 23:00:59 +0900

    [Feat] SearchTextFieldView 기본적인 UI 및 기능 추가

--- InSeoulHotPlace-iOS/Sources/Home/View/SearchTextFieldView.swift
+++ InSeoulHotPlace-iOS/Sources/Home/View/SearchTextFieldView.swift
@@ -5,4 +5,55 @@
 //  Created by 이지석 on 2023/07/25.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+protocol SearchTextFieldDelegate: AnyObject {
+    func searchText(title: String)
+}
+
+final class SearchTextFieldView: UIView {
+    
+    private let searchTextField = UITextField().then {
+        $0.placeholder = "원하는 장소를 입력해주세요"
+        $0.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+    }
+    
+    private lazy var disposeBag = DisposeBag()
+    weak var delegate: SearchTextFieldDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        setupLayouts()
+        setupBinding()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Setup helper
+private extension SearchTextFieldView {
+    func setupViews() {
+        addSubview(searchTextField)
+    }
+    
+    func setupLayouts() {
+        searchTextField.snp.makeConstraints {
+            $0.top.leading.bottom.trailing.equalToSuperview().inset(8)
+        }
+    }
+    
+    func setupBinding() {
+        searchTextField.rx.text.orEmpty
+            .asDriver()
+            .drive(onNext: { [weak self] text in
+                self?.delegate?.searchText(title: text)
+            })
+            .disposed(by: disposeBag)
+    }
+}
