4f756b3bdf369e1e7848aca95ab01f68de9dfc8f
Jiseok97 <wltjr1426@naver.com>
2023-07-25 23:25:07 +0900

    [Feat] Location Reponse Model 정의

--- InSeoulHotPlace-iOS/Sources/Home/Model/LocationModel.swift
+++ InSeoulHotPlace-iOS/Sources/Home/Model/LocationModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+struct LocationResponse: Codable {
+    let pageRange: [Int]
+    let total: Int
+    let row: [LocationData]
+}
+
+struct LocationData: Codable {
+    let areaNm: String
+    let congestionColor: String
+    let areaCongestLvl: String
+    let areaCongestNum: Int
+    let category: String
+    let x, y: String
+
+    enum CodingKeys: String, CodingKey {
+        case areaNm = "area_nm"
+        case congestionColor = "congestion_color"
+        case areaCongestLvl = "area_congest_lvl"
+        case areaCongestNum = "area_congest_num"
+        case category, x, y
+    }
+}
