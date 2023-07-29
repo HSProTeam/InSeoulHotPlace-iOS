dbf53c670e157017642e5961b2a6de00687680b9
Jiseok97 <wltjr1426@naver.com>
2023-07-29 12:05:44 +0900

    [Del] APIConstant getCategory path

--- InSeoulHotPlace-iOS/Sources/Common/APIConstant.swift
+++ InSeoulHotPlace-iOS/Sources/Common/APIConstant.swift
@@ -7,7 +7,4 @@
 
 struct APIConstant {
     static let baseURL = "https://data.seoul.go.kr/SeoulRtd/getCategoryList"
-    
-    // Basic path
-    static let getCategory = "?page=1&category=전체보기&count=113&sort=true"
 }
