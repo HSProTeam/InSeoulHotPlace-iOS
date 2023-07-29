1feec79d5d61c71a6c2a6b84a68f615d8f6e2aef
Jiseok97 <wltjr1426@naver.com>
2023-07-29 12:18:29 +0900

    [Feat] LocationViewModel rxmoya binding

--- InSeoulHotPlace-iOS/Sources/Home/ViewModel/LocationViewModel.swift
+++ InSeoulHotPlace-iOS/Sources/Home/ViewModel/LocationViewModel.swift
@@ -5,4 +5,33 @@
 //  Created by 이지석 on 2023/07/25.
 //
 
-import Foundation
+import Moya
+import RxMoya
+import RxSwift
+import RxCocoa
+import RxRelay
+
+final class LocationViewModel {
+    private lazy var disposeBag = DisposeBag()
+    private let provider = MoyaProvider<GetLocationService>()
+    private let locationRelay = BehaviorRelay<[LocationData]>(value: Array.init())
+    
+    var locationData: Driver<[LocationData]> {
+        return locationRelay.asDriver()
+    }
+    
+    func fetchLocationRequest() {
+        provider.rx.request(.getLocationService)
+            .subscribe { [weak self] result in
+                switch result {
+                case .success(let response):
+                    guard let responseData = try? response.map(LocationResponse.self) else { return }
+                    self?.locationRelay.accept(responseData.row)
+                    
+                case .failure(let error):
+                    print("DEBUG: fetchLocationRequest error is \(error.localizedDescription)")
+                }
+            }
+            .disposed(by: disposeBag)
+    }
+}
