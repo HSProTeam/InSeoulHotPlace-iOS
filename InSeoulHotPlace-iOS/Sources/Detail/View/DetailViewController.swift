90947c3d5a462b22016231e83c63e3c95b63429c
Jiseok97 <wltjr1426@naver.com>
2023-09-12 15:32:46 +0900

    [Style] Add ui components for Detail View Controller

--- InSeoulHotPlace-iOS/Sources/Detail/View/DetailViewController.swift
+++ InSeoulHotPlace-iOS/Sources/Detail/View/DetailViewController.swift
@@ -11,6 +11,37 @@ import RxCocoa
 
 class DetailViewController: BaseViewController {
     
+    private let congestView = UIView().then {
+        $0.backgroundColor = .systemGray6
+        $0.layer.cornerRadius = 15
+    }
+    
+    private let congestLabelView = UIView().then {
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 15
+    }
+    private let congestLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        $0.textColor = .black
+        $0.textAlignment = .center
+    }
+    
+    private let ageRateView = UIView().then {
+        $0.backgroundColor = .white
+    }
+    
+    private let genderRateView = UIView().then {
+        $0.backgroundColor = .white
+    }
+    
+    private let instructionLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        $0.textColor = .black
+        $0.numberOfLines = 0
+    }
+    
+    
+    private lazy var disposeBag = DisposeBag()
     private var detailLocationData: LocationData?
     private var viewModel: DetailLocationViewModel?
     
@@ -36,10 +67,32 @@ class DetailViewController: BaseViewController {
     
     override func setupViews() {
         view.backgroundColor = .white
+        view.addSubview(congestView)
+        
+        congestView.addSubview(congestLabelView)
+        congestLabelView.addSubview(congestLabel)
+        congestLabelView.addSubview(instructionLabel)
+        
+        congestView.addSubview(instructionLabel)
     }
     
     override func setupLayouts() {
+        congestView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(8)
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
         
+        congestLabelView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview().inset(16)
+        }
+        congestLabel.snp.makeConstraints {
+            $0.top.leading.bottom.trailing.equalToSuperview().inset(8)
+        }
+        
+        instructionLabel.snp.makeConstraints {
+            $0.top.equalTo(congestLabelView.snp.bottom).offset(12)
+            $0.leading.bottom.trailing.equalToSuperview().inset(16)
+        }
     }
     
     override func setupBinding() {
